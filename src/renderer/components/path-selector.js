e8d7029bc1ab32f96bf7c66adda772201095a18a
Mathias Rasmussen <mathiasvr@gmail.com>
2018-09-28 17:10:21 +0200

    clean up preferences-page

--- src/renderer/components/path-selector.js
+++ src/renderer/components/path-selector.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const colors = require('material-ui/styles/colors')
 const electron = require('electron')
 const React = require('react')
@@ -30,7 +32,7 @@ class PathSelector extends React.Component {
 
   handleClick () {
     const opts = Object.assign({
-      defaultPath: this.props.value,
+      defaultPath: this.props.value && path.dirname(this.props.value),
       properties: [ 'openFile', 'openDirectory' ]
     }, this.props.dialog)
 
--- src/renderer/pages/preferences-page.js
+++ src/renderer/pages/preferences-page.js
@@ -1,4 +1,3 @@
-const path = require('path')
 const React = require('react')
 
 const colors = require('material-ui/styles/colors')
@@ -99,7 +98,7 @@ class PreferencesPage extends React.Component {
           displayValue={playerName}
           onChange={this.handleExternalPlayerPathChange}
           title='External player'
-          value={playerPath ? path.dirname(playerPath) : null} />
+          value={playerPath} />
       </Preference>
     )
   }
@@ -132,11 +131,11 @@ class PreferencesPage extends React.Component {
     dispatch('updatePreferences', 'autoAddTorrents', isChecked)
 
     if (isChecked) {
-      dispatch('startFolderWatcher', null)
+      dispatch('startFolderWatcher')
       return
     }
 
-    dispatch('stopFolderWatcher', null)
+    dispatch('stopFolderWatcher')
   }
 
   torrentsFolderPathSelector () {
@@ -149,10 +148,9 @@ class PreferencesPage extends React.Component {
             title: 'Select folder to watch for new torrents',
             properties: [ 'openDirectory' ]
           }}
-          displayValue={torrentsFolderPath || ''}
           onChange={this.handletorrentsFolderPathChange}
           title='Folder to watch'
-          value={torrentsFolderPath ? path.dirname(torrentsFolderPath) : null} />
+          value={torrentsFolderPath} />
       </Preference>
     )
   }
